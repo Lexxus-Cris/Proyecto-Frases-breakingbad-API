3a08fc3fdc4385a588e55cc84c4fe8cdfc94bc11
Uriel Cristino <urielcris.97@gmail.com>
2021-08-20 16:25:13 -0500

    Obteniendo las frases desde la API

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import { Button, Container } from "./styles";
 
 function App() {
 
-  const consultarApi = () => {
-    console.log('consultando');
-  }
-  return (
+  const API = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+
+
+    const consultarApi = async () => {
+      try {
+        const respose = await fetch(API);
+        const data = await respose.json()
+        console.log(data[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  return(
     <Container>
       <Button
         onClick={consultarApi}
